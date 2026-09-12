b71cee5bb6dbf02416c5da57ae0e5edbcd744afb
ARYAN0-WORK <carrer1270@gmail.com>
2026-09-12 10:13:52 +0000

    feat: adding controllers for all crud operations

---
+++ Task-1/src/controllers/event.controller.ts
@@ -0,0 +1,51 @@
+import {Request,Response} from "express";
+
+import { createEvent, getAllEvents, getEventById,updateEvent, deleteEvent } from "../services/event.service.js";
+
+export const createEventController = async(
+    req: Request,
+    res: Response
+) => {
+    const event = await createEvent(req.body);
+
+    res.status(201).json({
+        success: true,
+        data: event,
+    })
+};
+
+export const getAllEventsController = async(
+    req: Request,
+    res: Response
+) => {
+    const events = await getAllEvents();
+
+    res.status(200).json({
+        success:true,
+        data: event
+    })
+}
+
+export const updateEventController = async(
+    req:Request,
+    res:Response
+) =>{
+    const event = await updateEvent(req.params.id as string, req.body);
+
+    res.status(200).json({
+        success: true,
+        data: event,
+    })
+}
+
+export const deleteEventContoller = async(
+    req: Request,
+    res: Response
+)=> {
+    const event = await deleteEvent(req.params.id as string);
+
+    res.status(200).json({
+        success: true,
+        data: event,
+    })
+}
