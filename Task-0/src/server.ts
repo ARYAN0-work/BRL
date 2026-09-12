9f6586cdee0c2eb392df8a56ce44024afb87b6c3
ARYAN0-WORK <carrer1270@gmail.com>
2026-09-12 07:21:38 +0000

    fixing architecture

---
+++ Task-0/src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { Request,Response } from "express";
+const app= express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/api/hello",(req:Request,res:Response)=>{
+   res.status(200).json({
+    "message":"Hello from luffy",
+    "success":"true"
+  })
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server's listening on ${PORT}`);
+})
