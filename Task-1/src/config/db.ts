e6ab0941089df3a7b06838be684aaf015db50d75
ARYAN0-WORK <carrer1270@gmail.com>
2026-09-12 08:24:48 +0000

    feat: adding database configuration file

---
+++ Task-1/src/config/db.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const connectDb = async () : Promise<void> =>{
+    try {
+        const mongoURI = process.env.MONGODB_URI;
+
+        if (!mongoURI) {
+            throw new Error("MONGODB_URI is not connected");
+        }
+
+        await mongoose.connect(mongoURI)
+
+        console.log("MongoDB connected");
+        
+    } catch (error) {
+        console.log("MongoDB Connection Failed:",error);
+        process.exit(1);
+    }
+}
+
+export default connectDb;
