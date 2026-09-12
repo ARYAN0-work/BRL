e7f6fd534779eb1d3553952c6fcdbe0b66477698
ARYAN0-WORK <carrer1270@gmail.com>
2026-09-12 08:28:29 +0000

    feat: adding connection between server and configuration files

--- Task-1/src/server.ts
+++ Task-1/src/server.ts
@@ -1,7 +1,15 @@
+import "dotenv/config"
 import app from "./app.js";
+import connectDb from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
+
+const startServer = async() : Promise<void>=>{
+    await connectDb();
+}
 
 app.listen(PORT,()=>{
     console.log(`Server's Running on ${PORT}`);  
 })
+
+startServer();
